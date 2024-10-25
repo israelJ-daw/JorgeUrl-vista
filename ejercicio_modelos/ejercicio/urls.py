e0142a5473e10b5fe3ceca85891f532da661310e
Israel Jimenez <israel.jimenez.jimenez.al@iespoligonosur.org>
2024-10-25 14:43:08 +0200

    todosss

--- ejercicio_modelos/ejercicio/urls.py
+++ ejercicio_modelos/ejercicio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ejercicio/listar', views.listar_proyectos, name='listar_proyectos')
+    path('ejercicio/listar', views.listar_proyectos, name='listar_proyectos'),
+    path('tarea/desc', views.tareas_desce, name='tarea_desce'),
 ]
--- ejercicio_modelos/ejercicio/views.py
+++ ejercicio_modelos/ejercicio/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from .models import *
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def listar_proyectos(request):
-    proyecto=Proyecto.all()
+def listar_proyectos(request):  
+    proyectos =Proyecto.objects.select_related('creador').prefetch_related('colaboradores')
+    proyectos=proyectos.all() 
     
-    return render(request, 'listar_proyectos.html', {"listar_proyectos" : proyecto })
+    
+    return render(request, 'listar_proyectos.html', {"listar_proyectos" : proyectos})
+
+def tareas_desce(request,proyecto_id):
+    tareas=Tarea.objects.select_related('proyecto')
+    tareas = tareas.filter(proyecto=proyecto_id).order_by("-fecha_creacion").all()
+    return render(request, 'tarea_desce.html', {"tareas_desce:": tareas}), 
+#nos falta hacer el template de aqui 
+    
